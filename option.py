5412e737e5ffbe57d87066a68b37736ed6ccfebf
FFFEI <60626746+feizhai39@users.noreply.github.com>
2022-01-08 10:22:00 -0500

    Update option.py

--- option.py
+++ option.py
@@ -2,7 +2,7 @@
 @project       : Queens College CSCI 365/765 Computational Finance
 @Instructor    : Dr. Alex Pang
 
-@Student Name  : first last
+@Student Name  : Fei Zhai
 
 @Date          : June 2021
 
